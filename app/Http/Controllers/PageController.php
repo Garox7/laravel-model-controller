deb21a425f2e82275c5d4ec8d8e92a0980686427
Garox7 <110902920+Garox7@users.noreply.github.com>
2023-01-16 16:29:28 +0100

    commit for check

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Movie;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
     public function index()
     {
+        $movies = Movie::all();
+        dump($movies);
         return view('guest.homepage');
     }
 }
